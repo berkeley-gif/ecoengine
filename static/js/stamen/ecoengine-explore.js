93e133f069da9fc03970d9e239459ffbc8e893a4
shoeswat <ymondal@berkeley.edu>
2015-04-23 08:30:16 -0700

    test checklist 5

--- static/js/stamen/ecoengine-explore.js
+++ static/js/stamen/ecoengine-explore.js
@@ -1230,6 +1230,9 @@
           .attr("href", ECO.endpoints.observations + "?format=geojson" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString);
         d3.select("#export-checklist")
           .attr(ECO.endpoints.observations + "?format=csv&fields=scientific_name" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString);
+console.log(d3.select("#export-checklist")
+          .attr(ECO.endpoints.observations + "?format=csv&fields=scientific_name" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString);)
+console.log(ECO.endpoints.observations + "?format=csv&fields=scientific_name" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString)
 
         loadingCheck("observations");
         d3.select("#results-loading")
