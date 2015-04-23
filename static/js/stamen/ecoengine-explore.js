395dcab8c5c5802fd39ebf7658d1d85dcecf4e0e
shoeswat <ymondal@berkeley.edu>
2015-04-23 08:36:40 -0700

    test checklist 9

--- static/js/stamen/ecoengine-explore.js
+++ static/js/stamen/ecoengine-explore.js
@@ -1230,11 +1230,9 @@
           .attr("href", ECO.endpoints.observations + "?format=geojson" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString);
         d3.select("#export-checklist")
           .attr(ECO.endpoints.observations + "?format=csv&fields=scientific_name" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString);
-console.log(d3.select("#export-checklist")
-          .attr(ECO.endpoints.observations + "?format=csv&fields=scientific_name" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString))
+console.log(d3.select("#export-checklist"))
 console.log(ECO.endpoints.observations + "?format=csv&fields=scientific_name" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString)
-console.log(d3.select("#export-geojson")
-          .attr("href", ECO.endpoints.observations + "?format=geojson" + orderString + facetstring + qString + bboxString + dateString + "&page_size=" + queryObj.page_size + pageString))
+console.log(d3.select("#export-geojson"))
 
         loadingCheck("observations");
         d3.select("#results-loading")
