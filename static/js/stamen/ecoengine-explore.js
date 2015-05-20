fefd206383f7be21c4e2a72a020cef1e71e5be82
shoeswat <ymondal@berkeley.edu>
2015-05-20 16:15:34 -0700

    test for 120

--- static/js/stamen/ecoengine-explore.js
+++ static/js/stamen/ecoengine-explore.js
@@ -731,7 +731,9 @@
           var fieldplot = d3.select("#search-bars")
             .html("")
             .selectAll("div.field-plot")
-            .data(searchfields.map(function(key) { console.log(data.fields['resource'].splice(2, 1)); return {
+            .data(searchfields.map(function(key) {
+            data.fields['resource'].splice(2, 1) /// REMOVE WHEN RASTERS ARE ADDED, THIS IS A CUSOM FIX!!! Holos issue #120
+            return {
               "field": key,
               "values": data.fields[key]
             }}))
